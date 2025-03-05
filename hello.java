1f1a54e34fefec7c0054af990c6c24de27dece75
Isaac <2393158@cognizant.com>
2025-03-05 09:50:17 +0530

    modified the java file

--- hello.java
+++ hello.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 class hello{
     public static void main(String []args){
         Scanner sc =new Scanner(System.in);
-        System.out.println("hello world");
+        System.out.println("enter a num");
+        int i = sc.nextInt();
+        System.out.println("hello world -" + i);
     }
 }
