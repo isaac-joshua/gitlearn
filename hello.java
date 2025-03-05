a632690902e9bbafcfcbfd7f2c12b1df95eaebac
Isaac <2393158@cognizant.com>
2025-03-05 14:12:00 +0530

    modifications

--- hello.java
+++ hello.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 class hello{
     public static void main(String []args){
-        Scanner sc =new Scanner(System.in);
-        System.out.println("enter a num");
-        int i = sc.nextInt();
-        System.out.println("hello world -" + (i+10));
-        System.out.println("hello world -" + (i*10));
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("enter a num");
+            int i = sc.nextInt();
+            System.out.println("hello world -" + (i+10));
+            System.out.println("hello world -" + (i*10));
+        }
     }
 }
